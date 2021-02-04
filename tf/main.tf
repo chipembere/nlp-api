0f8fcc580dc4b244a07df61455268822c4351715
admin <admin@INDXGBR043.local>
2021-02-05 02:06:15 +0300

    Update main.tf k8s deployment latest syntax.

--- tf/main.tf
+++ tf/main.tf
@@ -107,7 +107,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
+  # load_config_file       = false
 }
 
 resource "kubernetes_deployment" "nlp-api" {
@@ -140,11 +140,11 @@ resource "kubernetes_deployment" "nlp-api" {
           name  = "nlp-api"
 
           resources {
-            limits {
+            limits = {
               cpu    = "0.5"
               memory = "512Mi"
             }
-            requests {
+            requests = {
               cpu    = "250m"
               memory = "50Mi"
             }
