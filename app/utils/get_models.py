172ef18c9920a3dfc573dc04904125fd95936ef3
admin <admin@INDXGBR043.local>
2021-02-27 14:13:34 +0000

    specify model pwd.

--- app/utils/get_models.py
+++ app/utils/get_models.py
@@ -1,13 +1,14 @@
 def main():
     """ Downloads and saves nlp model and tokenizer in the same directory. """
+    import os
     from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
     tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
     model = AutoModelForSequenceClassification.from_pretrained(
                                         "ProsusAI/finbert")
-
-    tokenizer.save_pretrained("models/ProsusAI/finbert")
-    model.save_pretrained("models/ProsusAI/finbert")
+    pwd = os.getcwd()
+    tokenizer.save_pretrained(f"{pwd}/models/ProsusAI/finbert")
+    model.save_pretrained(f"{pwd}/models/ProsusAI/finbert")
 
 
 if __name__ == '__main__':
