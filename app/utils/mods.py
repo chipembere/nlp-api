ebc20b2b87b17feca34ac91e9f3d177cd55078ea
admin <admin@INDXGBR043.local>
2021-02-27 14:16:12 +0000

    specify model pwd.

--- app/utils/mods.py
+++ app/utils/mods.py
@@ -1,13 +1,17 @@
+import os
 from transformers import AutoTokenizer, AutoModelForSequenceClassification,\
                         pipeline
+
+pwd = os.getcwd()
 
 
 class MODS:
     def __init__(self, mod):
         self.mod = mod
-        self.tokenizer = AutoTokenizer.from_pretrained(f"models/{self.mod}")
+        self.tokenizer = AutoTokenizer.from_pretrained(
+                                f"{pwd}/models/{self.mod}")
         self.model = AutoModelForSequenceClassification.from_pretrained(
-            f"models/{self.mod}")
+            f"{pwd}/models/{self.mod}")
 
     def pipeline_sent(self, text: str):
         classifier = pipeline('sentiment-analysis', model=self.model,
